0ea79efc3bb5e388ac0aa7c2105c1af42e1fb162
codeinsack <codeinsack@gmail.com>
2020-10-12 00:51:50 +0300

    create basic client

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    Hello!
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import axios from 'axios';
+
+export default Vue.extend({
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  async mounted() {
+    const response = await axios.get('/api/v1/words');
+    console.log('response', response);
+  }
+});
+</script>
+
+<style scoped></style>
