6ba8c83b2f58d22c5c08d29c7f70b2c6e1b03e6c
codeinsack <codeinsack@gmail.com>
2020-10-14 14:13:56 +0300

    set up nginx

--- server/server.js
+++ server/server.js
@@ -1,13 +1,8 @@
 require('colors');
 const express = require('express');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
-const mongoose = require('mongoose');
-
-// Load env vars
-dotenv.config({ path: './config/config.env' });
 
 // Connect to database
 connectDB();
@@ -30,7 +25,7 @@ app.use('/api/v1/words', words);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT | 8080;
+const PORT = process.env.PORT | 5000;
 
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.red.bold));
 
