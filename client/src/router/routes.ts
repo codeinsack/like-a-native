4152f2cf19e88b30f0608fc1f99efe20763d3571
codeinsack <codeinsack@gmail.com>
2020-11-11 20:15:04 +0300

    add protected routes fe

--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -7,6 +7,13 @@ import UpdateWord from '@/views/UpdateWord/UpdateWord.vue';
 import Training from '@/views/Training/Training.vue';
 import Login from '@/views/Login/Login.vue';
 import Chat from '@/views/Chat/Chat.vue';
+import { useStore } from '@/uses/useStore';
+import { Modules } from '@/store/types';
+import { States } from '@/store/modules/user/types';
+
+const { useState } = useStore(Modules.USER);
+
+const { user } = useState([States.user]);
 
 Vue.use(VueRouter);
 
@@ -15,22 +22,37 @@ const routes = [
     path: '/words',
     component: Words,
     alias: '/',
+    meta: {
+      protected: true,
+    },
   },
   {
     path: '/words/details/:id',
     component: WordDetails,
+    meta: {
+      protected: true,
+    },
   },
   {
     path: '/words/update/:id',
     component: UpdateWord,
+    meta: {
+      protected: true,
+    },
   },
   {
     path: '/training',
     component: Training,
+    meta: {
+      protected: true,
+    },
   },
   {
     path: '/chat',
     component: Chat,
+    meta: {
+      protected: true,
+    },
   },
   {
     path: '/login',
@@ -39,4 +61,18 @@ const routes = [
   },
 ];
 
-export default new VueRouter({ mode: 'history', routes });
+const router = new VueRouter({ mode: 'history', routes });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.protected)) {
+    if (user.value) {
+      next();
+    } else {
+      next('/login');
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
